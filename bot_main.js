4392222056758b1d971f33d0e047dae075d2fbc6
A. L. Chalker <scribeklio@gmail.com>
2025-10-28 13:44:42 -0400

    initial setup and logic

---
+++ bot_main.js
@@ -0,0 +1,40 @@
+import { AtpAgent } from "@atproto/api";
+import * as dotenv from 'dotenv';
+import * as process from "process";
+import generateProgress from "./progress_generation.js";
+
+dotenv.config();
+
+// async function agentInit() { 
+//     const agent = new AtpAgent({
+//         service: "https://bsky.social", 
+//     });
+
+//     await agent.login({
+//         identifier: process.env.BLUESKY_USERNAME,
+//         password: process.env.BLUESKY_PASSWORD,
+//     });
+
+//     return agent; 
+// }
+
+// async function postIndicator() {
+//     // try {
+//     //     const agent = await agentInit();
+
+//         const progress = await generateProgress(); 
+
+//     //     await agent.post({
+//     //         text: progress, 
+//     //         createdAt: new Date().toISOString(),
+//     //     });
+
+//     //     console.log('post posted!!!', progress);
+//     // } catch (error) {
+//     //     console.error('porblem', error);
+//     // }
+// }
+
+// postIndicator();
+
+generateProgress();
---
+++ progress_generation.js
@@ -0,0 +1,55 @@
+
+function calculatePercentage() {
+    const inaugurationDay = new Date('2025-01-20');
+    const todaysDate = new Date(); 
+
+    const milisecondsSinceInaug = todaysDate - inaugurationDay; 
+
+    const milisecondsPerDay = 1000 * 60 * 60 * 24; 
+
+    const daysSinceInaug = Math.floor(milisecondsSinceInaug / milisecondsPerDay) 
+
+    const dayPercentageOfCentury = 0.0027; 
+
+    const progress = daysSinceInaug * dayPercentageOfCentury;
+
+    return progress; 
+}
+
+
+async function generateProgressBar () {
+    const progressPercentage = calculatePercentage() * 100;
+    const roundToNearestFive = Math.floor(progressPercentage / 5) * 5;
+
+    const fullBar = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒';
+
+    const filledSegments = roundToNearestFive / 5; 
+    const unfilledSegments = fullBar.length - filledSegments;
+    const padEndBound = fullBar.slice(unfilledSegments - 1);
+
+    let bar; 
+
+    if (padEndBound) {
+        bar = padEndBound.padEnd(fullBar.length, '░');
+    }
+
+    return bar;
+} 
+
+
+
+export default async function generateProgress() {
+    const progressBar = await generateProgressBar();
+
+    const progressPercentage = calculatePercentage() * 100;
+    const yearNumber = Math.floor(calculatePercentage()) + 1; 
+
+    const label = `${progressPercentage}% of ${yearNumber}% (year ${yearNumber}) `;
+
+    const message = ` ${progressBar} ${label}`; 
+
+    console.log('message', message);
+
+    /* ##### 75% of 1% (year 1) */
+    return message; 
+}
